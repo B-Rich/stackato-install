69ba3bca571082803c571175e8e9c5d6ffdec23f
Ingy döt Net <ingy@ingy.net>
2012-03-13 19:22:32 -0700

    bork teh regex

--- stackato-install-microcloud-vbox.sh
+++ stackato-install-microcloud-vbox.sh
@@ -48,7 +48,7 @@ function verify() {
     want_cmd 'wget'
 
     # Extract the active network device name from ifconfig
-    NET_DEVICE=`ifconfig | perl -e '$/="";for(<>){$d=${[split]}[0];if($d!~/^(lo|ppp0)/ and /inet addr+)/){print $d;last}}'`
+    NET_DEVICE=`ifconfig | perl -e '$/="";for(<>){$d=${[split]}[0];if($d!~/^(lo|ppp0)/ and /inet addr:(\S+)/){print $d;last}}'`
     if [ -z $NET_DEVICE ]; then
         die "Error: No network device seems to be active."
     fi
